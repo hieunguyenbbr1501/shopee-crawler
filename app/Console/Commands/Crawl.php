3d2724773f6204060fe59dfe4977e72796e65c40
hieu nguyen <hieunguyen.bbr1501@gmail.com>
2021-03-02 01:30:09 +0700

    first commit

---
+++ app/Console/Commands/Crawl.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Console\Command;
+
+class Crawl extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl:begin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Crawl data';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $keyword = "test";
+        $quantity = "50";
+//        $link = "https://shopee.vn/api/v2/search_items/?by=relevancy&keyword=".$keyword."&limit=".$quantity."&newest=0&order=desc&page_type=search";
+//        $client = new Client();
+//        $response = $client->request('GET', $link);
+//        echo $response;
+        $response = $this->LoginShopee('hieu15011', 'Thangnao?123', $keyword, $quantity);
+        dd($response["items"]);
+
+        return 0;
+    }
+
+    function LoginShopee($SP_Username, $SP_Pass, $keyword, $quantity)
+    {
+
+        //Khởi tạo chung cho toàn bộ request bên dưới
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0");
+        $cookie_jar = 'cookie_shopee.txt';
+        curl_file_create($cookie_jar);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_jar);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_jar);
+
+
+        // Request lấy cookie ban đầu
+        curl_setopt($ch, CURLOPT_URL, "https://shopee.vn/api/v0/buyer/login/");
+        curl_exec($ch);
+        // echo file_get_contents($cookie_jar) .'<br><br>';
+
+
+        //Khởi tạo các dữ liệu đầu vào cho request Login
+        $csrf_token = $this->csrftoken();
+
+        $header = array(
+            'x-csrftoken: ' . $csrf_token,
+            'x-requested-with: XMLHttpRequest',
+            'referer: https://shopee.vn/api/v0/buyer/login/',
+        );
+
+        $data = array(
+            "login_key" => $SP_Username,
+            "login_type" => "username",
+            "password_hash" => $this->CryptPass($SP_Pass),
+            "captcha" => "",
+            "remember_me" => "true"
+        );
+        $data = http_build_query($data);
+
+        //Request login
+        curl_setopt($ch, CURLOPT_URL, "https://shopee.vn/api/v0/buyer/login/login_post/");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, false);
+        curl_setopt($ch, CURLOPT_COOKIE, "csrftoken=" . $csrf_token);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//        curl_setopt($ch, CURLOPT_POSTFIELDSIZE, strlen($data));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $response = curl_exec($ch);
+
+        // Request lấy thông tin tài khoản sau khi đã đăng nhập thành công
+        curl_setopt($ch, CURLOPT_URL, "https://banhang.shopee.vn/api/v2/login/");
+        $response = curl_exec($ch);
+        // echo $response;
+        curl_setopt($ch, CURLOPT_POST, 0);
+        echo "https://shopee.vn/api/v2/search_items/?by=relevancy&keyword=" . urlencode($keyword)
+            . "&limit=" . urlencode($quantity) . "&match_id=8595&newest=0&order=desc&page_type=search&version=2";
+        curl_setopt($ch, CURLOPT_URL, "https://shopee.vn/api/v2/search_items/?by=relevancy&keyword="
+            . urlencode($keyword) . "&limit=" . $quantity . "&match_id=8595&newest=0&order=desc&page_type=search&version=2");
+        $response = curl_exec($ch);
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+
+        return (json_decode($body, true));
+    }
+
+    function CryptPass($pass)
+    {
+        return hash("sha256", md5($pass));
+    }
+
+    function csrftoken()
+    {
+        $karakter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        $PanjangKarakter = strlen($karakter);
+        $acakString = '';
+        for ($i = 0; $i < 32; $i++) {
+            $acakString .= $karakter[rand(0, $PanjangKarakter - 1)];
+        }
+        return $acakString;
+    }
+}
---
+++ app/Http/Controllers/KeywordController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Keyword;
+use Illuminate\Http\Request;
+
+class KeywordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Keyword  $keyword
+     * @return \Illuminate\Http\Response
+     */
+    public function show($keyword)
+    {
+        //
+        $response = $this->LoginShopee('hieu15011', 'Thangnao?123', $keyword);
+        dd(($response["data"]));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Keyword  $keyword
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Keyword $keyword)
+    {
+        //
+    }
+
+    function LoginShopee($SP_Username, $SP_Pass, $keyword)
+    {
+
+        //Khởi tạo chung cho toàn bộ request bên dưới
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0");
+        $cookie_jar = 'cookie_shopee.txt';
+        curl_file_create($cookie_jar);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_jar);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_jar);
+
+
+        // Request lấy cookie ban đầu
+        curl_setopt($ch, CURLOPT_URL, "https://shopee.vn/api/v0/buyer/login/");
+        curl_exec($ch);
+        // echo file_get_contents($cookie_jar) .'<br><br>';
+
+
+        //Khởi tạo các dữ liệu đầu vào cho request Login
+        $csrf_token = $this->csrftoken();
+
+        $header = array(
+            'x-csrftoken: ' . $csrf_token,
+            'x-requested-with: XMLHttpRequest',
+            'referer: https://shopee.vn/api/v0/buyer/login/',
+        );
+
+        $data = array(
+            "login_key" => $SP_Username,
+            "login_type" => "username",
+            "password_hash" => $this->CryptPass($SP_Pass),
+            "captcha" => "",
+            "remember_me" => "true"
+        );
+        $data = http_build_query($data);
+
+        //Request login
+        curl_setopt($ch, CURLOPT_URL, "https://shopee.vn/api/v0/buyer/login/login_post/");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, false);
+        curl_setopt($ch, CURLOPT_COOKIE, "csrftoken=" . $csrf_token);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//        curl_setopt($ch, CURLOPT_POSTFIELDSIZE, strlen($data));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $response = curl_exec($ch);
+
+        // Request lấy thông tin tài khoản sau khi đã đăng nhập thành công
+        curl_setopt($ch, CURLOPT_URL, "https://banhang.shopee.vn/api/v2/login/");
+        $response = curl_exec($ch);
+        // echo $response;
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_URL, "https://banhang.shopee.vn/api/marketing/v3/pas/suggest/keyword/?SPC_CDS=1&SPC_CDS_VER=2&keyword=" .$keyword . "&count=100&placement=0&itemid=6967948430");
+        $response = curl_exec($ch);
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+
+        return (json_decode($body, true));
+        curl_close($ch);
+    }
+
+    function CryptPass($pass)
+    {
+        return hash("sha256", md5($pass));
+    }
+
+    function csrftoken()
+    {
+        $karakter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        $PanjangKarakter = strlen($karakter);
+        $acakString = '';
+        for ($i = 0; $i < 32; $i++) {
+            $acakString .= $karakter[rand(0, $PanjangKarakter - 1)];
+        }
+        return $acakString;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Keyword  $keyword
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Keyword $keyword)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Keyword  $keyword
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Keyword $keyword)
+    {
+        //
+    }
+}
