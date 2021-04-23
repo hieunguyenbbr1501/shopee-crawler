d8bd109d11ce3fa52273c6c84325be502a7397de
hieu nguyen <hieunguyen.bbr1501@gmail.com>
2021-04-23 23:37:23 +0700

    update

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -2,7 +2,9 @@
 $(document).ready(function() {
   $('#dataTable').DataTable( {
       "language": {
-          "search": "Tìm kiếm:"
+          "search": "Tìm kiếm:",
+          "lengthMenu" : "Hiển thị _MENU_ kết quả",
+          "info" : "Hiển thị trang _PAGE_ trong tổng số _PAGES_ trang"
       }
       }
 
